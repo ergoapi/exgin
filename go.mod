f5f80777f8a5b9e0533c223fe59ceb9db44948a2
ysicing <i@ysicing.me>
2021-08-03 14:33:32 +0800

    feat(init): init
    init
    
    Signed-off-by: ysicing <i@ysicing.me>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ergoapi/exgin
+
+go 1.16
+
+require (
+	github.com/ergoapi/errors v0.0.0-20210803061602-3f0392c79a5f
+	github.com/ergoapi/util v0.0.0-20210803062902-9c2dbfdac7e2
+	github.com/gin-gonic/gin v1.7.3
+	github.com/google/gops v0.3.19
+)
---
+++ resp.go
@@ -0,0 +1,76 @@
+//  Copyright (c) 2021. The EFF Team Authors.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package exgin
+
+import (
+	"fmt"
+
+	"github.com/ergoapi/errors"
+	"github.com/ergoapi/util/ztime"
+	"github.com/gin-gonic/gin"
+)
+
+// done done
+func respdone(data interface{}) gin.H {
+	return gin.H{
+		"data":      data,
+		"message":   "请求成功",
+		"timestamp": ztime.NowUnix(),
+		"code":      200,
+	}
+}
+
+// error error
+func resperror(code int64, data interface{}) gin.H {
+	return gin.H{
+		"data":      nil,
+		"message":   data,
+		"timestamp": ztime.NowUnix(),
+		"code":      code,
+	}
+}
+
+func renderMessage(c *gin.Context, v interface{}) {
+	if v == nil {
+		c.JSON(200, respdone(nil))
+		return
+	}
+
+	switch t := v.(type) {
+	case string:
+		c.JSON(200, resperror(10400, t))
+	case error:
+		c.JSON(200, resperror(10400, t.Error()))
+	}
+}
+
+func GinsData(c *gin.Context, data interface{}, err error) {
+	if err == nil {
+		c.JSON(200, respdone(data))
+		return
+	}
+
+	renderMessage(c, err.Error())
+}
+
+func GinsAbort(c *gin.Context, msg string, args ...interface{}) {
+	c.AbortWithStatusJSON(200, resperror(10400, fmt.Sprintf(msg, args...)))
+}
+
+func Bind(c *gin.Context, ptr interface{}) {
+	err := c.ShouldBindJSON(ptr)
+	if err != nil {
+		errors.Bomb("参数不合法: %v", err)
+	}
+}
